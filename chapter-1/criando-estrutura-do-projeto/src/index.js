4833c87da5124fabce0fe83c27d7340089e619a7
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-04-25 00:11:01 -0300

    add params types

--- chapter-1/criando-estrutura-do-projeto/src/index.js
+++ chapter-1/criando-estrutura-do-projeto/src/index.js
@@ -2,9 +2,48 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/',(request, response)=>{
     return response.json({message:'hello world'})
 });
+
+
+//route params
+app.get('/search/users',(req, res)=>{
+    const user1 = {
+        name:'andre'
+
+    }
+    const user2 = {
+        name:'vk'
+
+    }
+    return res.json({user1, user2});
+})
+
+
+app.get('/search/user/:name',(req, res)=>{
+    const name = req.query;
+    console.log(name);
+    
+    const user1 = {
+        name:'andre'
+
+    }
+    const user2 = {
+        name:'vk'
+
+    }
+    let userSearch = 'not found';
+    if(name==user1.name){
+        userSearch = user1;
+    }else if(name==user2.name){
+        userSearch=user2.name;
+    }
+    return res.json({userSearch});
+})
+
 
 app.post("/register/use", (req, res)=>{
     return res.json({name:"andre"})

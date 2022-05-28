7bd43b1fa69b7c7d1fb0ae9b469dd10f120751aa
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-05-28 19:02:21 -0300

    middlewares, deposit

--- chapter-1/content/criando-primeira-aplicacao/src/index.js
+++ chapter-1/content/criando-primeira-aplicacao/src/index.js
@@ -4,8 +4,12 @@ const {v4: uuidv4} = require('uuid');
 const app = express();
 
 app.use(express.json());
+//app.use(verifyIfExistsAccountCpf);
 
-const customers = [];
+
+const customers = [
+    
+];
 
 app.post('/account',(request, response)=>{
     const {cpf, name} = request.body;
@@ -31,6 +35,26 @@ app.post('/account',(request, response)=>{
     return response.status(201).send();
 });
 
+app.post("/deposit",verifyIfExistsAccountCpf,(req,res)=>{
+    const {description, amount} = req.body;
+
+    const {customer}= req;
+
+    const statementOperation ={
+        description,
+        amount,
+        created_at: new Date(),
+        type:'credit'
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+
+
+
+})
+
 app.get('/statement/:cpf',(req,res)=>{
     const {cpf} = req.params;
 
@@ -42,19 +66,30 @@ app.get('/statement/:cpf',(req,res)=>{
     return res.json(customer.statement);
 })
 
-app.get('/statement/',(req,res)=>{
+app.get('/account',(req,res)=>{
+
+    
+    return res.json(customers);
+})
+
+//middleware
+function verifyIfExistsAccountCpf(req, res, next){
     const {cpf} = req.headers;
 
     const customer = customers.find((customer)=> customer.cpf === cpf);
 
     if(!customer){
-        return res.status(400).json({error:"Customer Not Found"})
-    }
-    return res.json(customer.statement);
+        return res.status(404).json({error:"Customer Not Found md"})
+    } 
+    req.customer = customer;
+
+    return next();
+}
+
+app.get('/statement/',verifyIfExistsAccountCpf, (req,res)=>{
+    return res.json(req.customer);
 })
 
 
 //route params
-
-
 app.listen(3001, ()=> console.log('start server on port 3001'));

199e5a132b0ceadc9143829377c9552c569a0fb5
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-16 21:39:59 -0300

    refactor(routes): refactor categories routes

--- chapter-2/content/criando-api-com-nodejs/src/routes/categoriesRoutes.ts
+++ chapter-2/content/criando-api-com-nodejs/src/routes/categoriesRoutes.ts
@@ -1,29 +1,23 @@
-import { Request, Response, Router } from 'express';
-import CategoryRepository from '../modules/cars/repositories/CategoryRepository';
-import { CreateCategoryService } from '../modules/cars/services/createCategoryService';
+import { Router } from 'express';
+
+import { createCategoryController } from '../modules/cars/useCases';
 
 const categoriesRoutes = Router();
 
-const categoryRepository = new CategoryRepository();
 
 // const categoryMiddlleware = new CategoryMiddlleware(categoryRepository);
 
-const createCategoryService = new CreateCategoryService(categoryRepository);
 
 // categoriesRoutes.use(categoryMiddlleware.verifyCategory);
 
 
 
-categoriesRoutes.post('/',(req:Request, res:Response)=>{
-    const {name, description} = req.body;
+categoriesRoutes.post('/',(req,res)=>{
+    return createCategoryController.create(req,res);
+});
 
-    createCategoryService.execute({name, description});
-
-    return res.status(201).send();
-})
-
-categoriesRoutes.get('/',(req:Request, res:Response)=>{
-    return res.json(categoryRepository.list());
-})
+// categoriesRoutes.get('/',(req:Request, res:Response)=>{
+//     return res.json(categoryRepository.list());
+// })
 
 export { categoriesRoutes };

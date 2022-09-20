89aec4897efc401bd16984b88bd9e7479239b9ef
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-20 16:20:21 -0300

    refactor: move specifications  specification  routes from ./routes/index.ts

--- chapter-2/content/criando-api-com-nodejs/src/routes/specificationsRoutes.ts
+++ chapter-2/content/criando-api-com-nodejs/src/routes/specificationsRoutes.ts
@@ -1,22 +1,15 @@
 import { Request, Response, Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationsService } from "../modules/cars/services/createSpecificationsService";
-
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
-const createSpecificationsService = new CreateSpecificationsService(specificationsRepository);
 
 
 specificationsRoutes.post('/',(req:Request,res:Response)=>{
-    const {name, description} = req.body;
 
-    createSpecificationsService.execute({name, description});
-
-    return res.status(201).send();
+    return createSpecificationController.create(req, res);
 
 })
 
-export { specificationsRoutes};
+export { specificationsRoutes };

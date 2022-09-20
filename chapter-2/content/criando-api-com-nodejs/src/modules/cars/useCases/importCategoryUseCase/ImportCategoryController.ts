1cf094741a5bf1d62a23b0d40d75c0ec05884060
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-20 17:28:38 -0300

    feat: add import useCases

---
+++ chapter-2/content/criando-api-com-nodejs/src/modules/cars/useCases/importCategoryUseCase/ImportCategoryController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+
+class ImportCategoryController{
+
+    private importCategoryUseCase: ImportCategoryUseCase;
+
+    constructor(importCategoryUseCase:ImportCategoryUseCase){
+        this.importCategoryUseCase = importCategoryUseCase;
+    }
+
+    import(req:Request, res:Response){
+        const {file} = req;
+    
+
+        this.importCategoryUseCase.execute(file);
+
+        return res.send();
+
+    }
+
+}
+
+export { ImportCategoryController}

f75057ff0108d616723216cf0aed002655b0aec3
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-20 16:22:57 -0300

    style: rename class

--- chapter-2/content/criando-api-com-nodejs/src/modules/cars/useCases/listCategory/ListCategoryController.ts
+++ chapter-2/content/criando-api-com-nodejs/src/modules/cars/useCases/listCategory/ListCategoryController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+import { ListCategoryUseCase } from "./ListCategoryUseCase";
 
 class ListCategoryController{
-    constructor (private listCategoryUseCase:ListCategoriesUseCase){}
+    constructor (private listCategoryUseCase:ListCategoryUseCase){}
 
     list(req:Request,res:Response){
         return res.status(200).json(this.listCategoryUseCase.execute());

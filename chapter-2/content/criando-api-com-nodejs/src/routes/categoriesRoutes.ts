6cae39b445ea81c8612c67444de9c73d981c8444
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-20 17:30:57 -0300

    feat: create route /import for file

--- chapter-2/content/criando-api-com-nodejs/src/routes/categoriesRoutes.ts
+++ chapter-2/content/criando-api-com-nodejs/src/routes/categoriesRoutes.ts
@@ -1,8 +1,16 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategoryUseCase';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
+
+
+const upload = multer({
+    dest:'./temp'
+})
 const categoriesRoutes = Router();
 
 categoriesRoutes.post('/',(req,res)=>{
@@ -11,6 +19,11 @@ categoriesRoutes.post('/',(req,res)=>{
 
 categoriesRoutes.get('/',(req, res)=>{
     return listCategoryController.list(req,res);
+})
+
+
+categoriesRoutes.post('/import', upload.single('filename'),(req:Request, res:Response)=>{
+    return importCategoryController.import(req,res);
 })
 
 export { categoriesRoutes };

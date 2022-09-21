940f9108b96517c00feaf914b60ca3e144dff7db
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-21 20:26:28 -0300

    feat: stream file

--- chapter-2/content/criando-api-com-nodejs/src/modules/cars/useCases/importCategoryUseCase/ImportCategoryUseCase.ts
+++ chapter-2/content/criando-api-com-nodejs/src/modules/cars/useCases/importCategoryUseCase/ImportCategoryUseCase.ts
@@ -1,9 +1,55 @@
+import fs from 'fs';
+import {parse} from 'csv-parse';
+import CategoryRepository from '../../repositories/CategoryRepository';
 
 
+interface IimportCategory{
+    name:string;
+    description:string;
+}
 class ImportCategoryUseCase{
 
-    execute(file:any){
-        console.log(file);
+
+
+    constructor(private categoryRepository:CategoryRepository){
+
+    }
+
+    loadCategories(file:Express.Multer.File):Promise<IimportCategory[]>{
+       
+       return new Promise((resolve,reject)=>{
+         const stream = fs.createReadStream(file.path);
+        const categories:IimportCategory[] = [];
+
+        const parseFile = parse({
+            delimiter:','
+        });
+
+
+        stream.pipe(parseFile);
+
+        parseFile.on('data', async (line)=>{
+            const [name, description] = line;
+
+            categories.push({
+                name,
+                description
+            })
+        })
+        .on('end',()=>{
+            resolve(categories);
+        })
+        .on('error',(err)=>{
+            reject(err);
+        })
+       })
+      
+    }
+    async execute(file:Express.Multer.File):Promise<void>{
+        const categories = await this.loadCategories(file);
+        console.log(categories);
+
+    
     }
 }
 

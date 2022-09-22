5b8150f49d25fc247340f06188c88c8053f1d550
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-22 18:34:12 -0300

    refactor: move for directory repositories

---
+++ chapter-2/content/criando-api-com-nodejs/src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,16 @@
+import { Specification } from "../models/Specification";
+
+
+interface IcreateSpecificationDTO{
+    name:string;
+    description:string;
+}
+
+interface ISpecificationRepository{
+
+    create({name, description}:IcreateSpecificationDTO):void;
+    findByName(name:string):Specification;
+
+}
+
+export { ISpecificationRepository, IcreateSpecificationDTO}
